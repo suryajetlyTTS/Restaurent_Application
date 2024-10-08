be25db170c9dbd4b591087b40ff1d8c73b872c79
VidhyabharathiK <146526431+VidhyabharathiK@users.noreply.github.com>
2024-10-08 12:39:55 +0530

    userlogin , adminlogin  added

--- RestaurentApp/src/app/admin/admin.module.ts
+++ RestaurentApp/src/app/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserloginComponent,
+    AdminloginComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
---
+++ RestaurentApp/src/app/admin/adminlogin/adminlogin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminlogin',
+  templateUrl: './adminlogin.component.html',
+  styleUrl: './adminlogin.component.css'
+})
+export class AdminloginComponent {
+
+}
